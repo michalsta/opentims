f036d85bcba4d835bbd678c50e707afc480bb343
Michał Startek <mist@mimuw.edu.pl>
2020-11-25 03:03:34 +0100

    Making the code less error prone

--- opentims/opentims.cpp
+++ opentims/opentims.cpp
@@ -128,9 +128,21 @@ void TimsFrame::save_to_buffs(uint32_t* frame_ids, uint32_t* scan_ids, uint32_t*
     if(num_peaks == 0)
         return;
 
-    assert(scan_ids != nullptr);
-    assert(tofs != nullptr);
-    assert(intensities != nullptr);
+    if(scan_ids == nullptr)
+    {
+        std::unique_pointer<uint32_t[]> scan_ids_hndl = std::make_unique<uint32_t>(num_peaks);
+        scan_ids = scan_ids_hndl.get();
+    }
+    if(tofs == nullptr)
+    {
+        std::unique_pointer<uint32_t[]> tofs_hndl = std::make_unique<uint32_t>(num_peaks);
+        tofs = tofs_hndl.get();
+    }
+    if(intensities == nullptr)
+    {
+        std::unique_pointer<uint32_t[]> intensities_hndl = std::make_unique<uint32_t>(num_peaks);
+        intensities = intensities_hndl.get();
+    }
 
     bool needs_closure = false;
     if(bytes0 == nullptr)

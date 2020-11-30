4d5da2750b856dca5ac33d224322e1e6a38206bc
MatteoLacki <matteo.lacki@gmail.com>
2020-11-30 11:34:58 +0100

    hashes work

--- opentimspy/dev/compare_cross_OS.py
+++ opentimspy/dev/compare_cross_OS.py
@@ -4,42 +4,40 @@ import itertools
 from collections import Counter
 import hashlib
 
-import opentims
-from test_dir.hashing import hash_frame, hash_dataset
+import opentimspy
+from opentimspy.opentims import OpenTIMS
 
-path = pathlib.Path('~/Projects/bruker/BrukerMIDIA/MIDIA_CE10_precursor/20190912_HeLa_Bruker_TEN_MIDIA_200ng_CE10_100ms_Slot1-9_1_488.d').expanduser().resolve()
+path = pathlib.Path("/home/matteo/Projects/bruker/BrukerMIDIA/MIDIA_CE10_precursor/20190912_HeLa_Bruker_TEN_MIDIA_200ng_CE10_100ms_Slot1-9_1_488.d")
 
-path = pathlib.Path('~/Projects/bruker/20190912_HeLa_Bruker_TEN_MIDIA_200ng_CE10_100ms_Slot1-9_1_488.d').expanduser().resolve()
-path.exists()
-
-def hash_frame(X):
+def hash_frame(X, columns=('frame','scan','tof','intensity')):
     hashes = []
-    for i in range(1,4):
+    for c in columns:
         h = hashlib.blake2b()
-        h.update(X[:,i])
+        h.update(X[c])
         hashes.append(h.digest())
     return hashes
 
+def hashes_dataset(D):
+    return [hash_frame(X) 
+            for X in D.query_iter(frames=slice(D.min_frame,
+                                               D.max_frame+1))]
+
+def hash_dataset(D, columns=('frame','scan','tof','intensity')):
+    h = hashlib.blake2b()
+    for X in D.query_iter(frames=slice(D.min_frame,
+                                       D.max_frame+1),
+                          columns=columns):
+        for c in columns:
+            h = hashlib.blake2b()
+            h.update(X[c])
+    return h.digest()
 
 D = OpenTIMS(path)
-X = D[2]
-h = hash_dataset(D)
-b'NfP\xbch\xeb\xc4\x9eG\xcf\xb2i\x83!\xfdX4\xc6\xd8\xe0?\\\xb5B\x90\x0e\x99[\xb5\xfa\x87\xfe<&\xe3F\x0b\x8e\xd8\x9a\xab\xad\xda\xf3\xb2\x83\xd9\x14\x91\xcf\x989\xe6\x12\xe0\xc3.\x0bf\xd6\xa0"\x96\xc0'
-
-with open('../hash', 'r') as f:
-    h_linux = f.read()
-
-h_linux == str(h)
+data_set_hash = hash_dataset(D)
+# b'-3?\x8e\x12&\xcbYd\xb0\xd6\x82MV)\xe7\xfc\x07Z\xa3\xf8\xe8\x0e\x9d\xda;\x13=\tt\x0fY\xea\x92e\x9e\x14\x90\xfb\x17{\x1e\xb3J\xd8\xf2&\x05I\x8c\xfbM: \x13\xbddJ\xf7\x9b\xc4x\\4'
 
 
-# H = [get_hashes(X) for X in D]
-# H = np.array(H)
 
-np.save('hashes.npy', H)
-H0 = np.load('hashes.npy')
-H1 = H0.copy()
-H1[100,2] = 'a'
-H0[100,2]
 
 def diff_hashes(H0, H1):
     assert H0.shape == H1.shape, "The elements dimensions differ."
@@ -47,19 +45,4 @@ def diff_hashes(H0, H1):
         for j, (a0, a1) in enumerate(zip(r0, r1)):
             if a0 != a1:
                 yield i,j
-
-list(diff_hashes(H0, H1))
-
-# main data on Windows
-path = pathlib.Path('C:/Projects/bruker_data/20190912_HeLa_Bruker_TEN_MIDIA_200ng_CE10_100ms_Slot1-9_1_488.d')
-D = OpenTIMS(path)
-h = hash_dataset(D)
-
-# another data-set
-path = pathlib.Path('C:/Projects/bruker_data/F2020-09-14_18-43-30_M200914_01_69.d')
-D = OpenTIMS(path)
-h = hash_dataset(D)
-# windows:
-# b'\x8b\x93\xb9\x8b\xa0\xca\x0e\xbf1\xf6\x0cd\x9e\x8b1&\xbeZG\x94\xa9\xce(\xb9Gy\xa5YP\xc4\xd6G\x96\xee\xa2\x0f\x1b\x00\x99\xa0FE\x00\xb2q\xd9$\xf2\xcf\xad\x89\xec\xe2\ri\x9by\x9fG\t\xa1yW\x87'
-
 

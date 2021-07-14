a2e223e1036639dc4135b85691bcb4634b8f2753
Michał Startek <mist@mimuw.edu.pl>
2021-07-15 00:43:24 +0200

    Convenience header

---
+++ opentims++/opentims_all.h
@@ -0,0 +1,7 @@
+#include "platform.h"
+#include "opentims.h"
+#include "bruker.h"
+#include "scan2inv_ion_mobility_converter.h"
+#include "so_manager.h"
+#include "tof2mz_converter.h"
+#include "converters.h"

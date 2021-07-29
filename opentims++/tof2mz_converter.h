1b7d734bc8df35aa2d3c7771f84f8d75b2b64a19
Michał Startek <mist@mimuw.edu.pl>
2021-07-29 09:35:31 +0200

    Refactor a bit more

--- opentims++/tof2mz_converter.h
+++ opentims++/tof2mz_converter.h
@@ -34,24 +34,17 @@ class Tof2MzConverter
  public:
     virtual void convert(uint32_t frame_id, double* mzs, const double* tofs, uint32_t size) = 0;
     virtual void convert(uint32_t frame_id, double* mzs, const uint32_t* tofs, uint32_t size) = 0;
-    virtual ~Tof2MzConverter() {};
-    virtual std::string description() { return "Tof2MzConverter default"; };
+    virtual ~Tof2MzConverter();
+    virtual std::string description();
 };
 
 class ErrorTof2MzConverter : public Tof2MzConverter
 {
  public:
-    ErrorTof2MzConverter(TimsDataHandle&) {};
-    void convert(uint32_t, double*, const double*, uint32_t) override final
-    {
-        throw std::logic_error("Default conversion method must be selected BEFORE opening any TimsDataHandles - or it must be passed explicitly to the constructor");
-    }
-
-    void convert(uint32_t, double*, const uint32_t*, uint32_t) override final
-    {
-        throw std::logic_error("Default conversion method must be selected BEFORE opening any TimsDataHandles - or it must be passed explicitly to the constructor");
-    }
-    std::string description() override final { return "ErrorTof2MzConverter default"; };
+    ErrorTof2MzConverter(TimsDataHandle&);
+    void convert(uint32_t, double*, const double*, uint32_t) override final;
+    void convert(uint32_t, double*, const uint32_t*, uint32_t) override final;
+    std::string description() override final;
 };
 
 class BrukerTof2MzConverter final : public Tof2MzConverter
@@ -64,58 +57,29 @@ class BrukerTof2MzConverter final : public Tof2MzConverter
     tims_close_fun_t* tims_close;
     tims_convert_fun_t* tims_index_to_mz;
 
-    std::string get_tims_error()
-    {
-        const size_t buf_size = 10000;
-        std::unique_ptr<char[]> buf = std::make_unique<char[]>(buf_size);
-        tims_get_last_error_string(buf.get(), buf_size-1);
-        buf[buf_size-1] = '\0';
-        return std::string(buf.get());
-    }
+    std::string get_tims_error();
 
  public:
-    BrukerTof2MzConverter(TimsDataHandle& TDH, const std::string& lib_path) : lib_handle(lib_path), bruker_file_handle(0)
-    {
-        tims_open = lib_handle.symbol_lookup<tims_open_fun_t>("tims_open");
-        tims_get_last_error_string = lib_handle.symbol_lookup<tims_get_last_error_string_fun_t>("tims_get_last_error_string");
-        tims_close = lib_handle.symbol_lookup<tims_close_fun_t>("tims_close");
-        tims_index_to_mz = lib_handle.symbol_lookup<tims_convert_fun_t>("tims_index_to_mz");
+    BrukerTof2MzConverter(TimsDataHandle& TDH, const std::string& lib_path);
+    ~BrukerTof2MzConverter();
 
-        bruker_file_handle = (*tims_open)(TDH.tims_dir_path.c_str(), 0); // Recalibrated states not supported
+    void convert(uint32_t frame_id, double* mzs, const double* tofs, uint32_t size) override final;
+    void convert(uint32_t frame_id, double* mzs, const uint32_t* tofs, uint32_t size) override final;
 
-        if(bruker_file_handle == 0)
-            throw std::runtime_error("tims_open(" + TDH.tims_dir_path + ") failed. Reason: " + get_tims_error());
-    }
-
-    ~BrukerTof2MzConverter() { if(bruker_file_handle != 0) tims_close(bruker_file_handle); }
-
-    void convert(uint32_t frame_id, double* mzs, const double* tofs, uint32_t size) override final
-    {
-        tims_index_to_mz(bruker_file_handle, frame_id, tofs, mzs, size);
-    }
-
-    void convert(uint32_t frame_id, double* mzs, const uint32_t* tofs, uint32_t size) override final
-    {
-        std::unique_ptr<double[]> dbl_tofs = std::make_unique<double[]>(size);
-        for(uint32_t idx = 0; idx < size; idx++)
-            dbl_tofs[idx] = static_cast<double>(tofs[idx]);
-        tims_index_to_mz(bruker_file_handle, frame_id, dbl_tofs.get(), mzs, size);
-    }
-
-    std::string description() override final { return "BrukerTof2MzConverter"; };
+    std::string description() override final;
 };
 
 class Tof2MzConverterFactory
 {
  public:
     virtual std::unique_ptr<Tof2MzConverter> produce(TimsDataHandle& TDH) = 0;
-    virtual ~Tof2MzConverterFactory() {};
+    virtual ~Tof2MzConverterFactory();
 };
 
 class ErrorTof2MzConverterFactory final : public Tof2MzConverterFactory
 {
  public:
-    std::unique_ptr<Tof2MzConverter> produce(TimsDataHandle& TDH) override final { return std::make_unique<ErrorTof2MzConverter>(TDH); };
+    std::unique_ptr<Tof2MzConverter> produce(TimsDataHandle& TDH) override final;
 };
 
 class BrukerTof2MzConverterFactory final : public Tof2MzConverterFactory
@@ -123,22 +87,16 @@ class BrukerTof2MzConverterFactory final : public Tof2MzConverterFactory
     const std::string dll_path;
     LoadedLibraryHandle lib_hndl;
  public:
-    BrukerTof2MzConverterFactory(const char* _dll_path) : dll_path(_dll_path), lib_hndl(_dll_path) {};
-    BrukerTof2MzConverterFactory(const std::string& _dll_path) : dll_path(_dll_path), lib_hndl(_dll_path) {};
-    std::unique_ptr<Tof2MzConverter> produce(TimsDataHandle& TDH) override final { return std::make_unique<BrukerTof2MzConverter>(TDH, dll_path.c_str()); };
+    BrukerTof2MzConverterFactory(const char* _dll_path);
+    BrukerTof2MzConverterFactory(const std::string& _dll_path);
+    std::unique_ptr<Tof2MzConverter> produce(TimsDataHandle& TDH) override final;
 };
 
 class DefaultTof2MzConverterFactory final
 {
     static std::unique_ptr<Tof2MzConverterFactory> fac_instance;
  public:
-    static std::unique_ptr<Tof2MzConverter> produceDefaultConverterInstance(TimsDataHandle& TDH)
-    {
-        if(!fac_instance)
-            fac_instance = std::make_unique<ErrorTof2MzConverterFactory>();
-
-        return fac_instance->produce(TDH);
-    }
+    static std::unique_ptr<Tof2MzConverter> produceDefaultConverterInstance(TimsDataHandle& TDH);
 
     template<class FactoryType, class... Args> static void setAsDefault(Args&& ... args)
     {

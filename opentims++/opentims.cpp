c1f987fcd2617db9c5f1d0573dae29f04bd6e742
Michał Startek <michal.startek@mimuw.edu.pl>
2021-11-30 16:10:43 +0100

    Cleanup

--- opentims++/opentims.cpp
+++ opentims++/opentims.cpp
@@ -434,7 +434,6 @@ TimsDataHandle(tims_data_dir)
 
 TimsDataHandle::~TimsDataHandle()
 {
-    std::cerr << "dfvhjsbdvhjbdsjvhfvb" << std::endl;
     if(zstd_dctx != nullptr)
         ZSTD_freeDCtx(zstd_dctx);
 #ifndef OPENTIMS_BUILDING_R

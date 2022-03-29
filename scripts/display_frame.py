986f22df48a76555d911a3af02972846d7bac717
Michał Startek <michal.startek@mimuw.edu.pl>
2022-03-29 22:18:40 +0200

    Smarter display_frame.py

--- scripts/display_frame.py
+++ scripts/display_frame.py
@@ -8,7 +8,7 @@ import numpy as np
 import argparse
 parser = argparse.ArgumentParser(description='Display a plot of a single frame from TDF file.')
 parser.add_argument("path", help="TDF dataset path", type=Path)
-parser.add_argument("frames", help="Comma-separated list of frames, including ranges. Example: 314,320-330,435. Will plot everything if omitted.", nargs='?', default="")
+parser.add_argument("frames", help="Comma-separated list of frames, including ranges. Example: 314,320-330,435. Will plot everything if omitted.", nargs='?', default=None)
 parser.add_argument("-s", "--save", help="Save the images to files instead of displaying them", action="store_true")
 parser.add_argument("--silent", help="do not display progressbar", action='store_true')
 args=parser.parse_args()
@@ -38,13 +38,16 @@ with OpenTIMS(args.path) as OT:
             plt.show()
 
 
-    frames = set()
-    for frame_desc in args.frames.split(','):
-        if '-' in frame_desc:
-            start, end = frame_desc.split('-')
-            frames.update(range(int(start), int(end)+1))
-        else:
-            frames.add(int(frame_desc))
+    if args.frames is None:
+        frames = set(OT.frame_properties.keys())
+    else:
+        frames = set()
+        for frame_desc in args.frames.split(','):
+            if '-' in frame_desc:
+                start, end = frame_desc.split('-')
+                frames.update(range(int(start), int(end)+1))
+            else:
+                frames.add(int(frame_desc))
 
     if len(frames) == 0:
         frames = D.frames['Id']

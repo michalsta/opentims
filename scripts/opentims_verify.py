9e7b0f436d5df561e3aef4da66d342949a814bb9
Michał Startek <michal.startek@mimuw.edu.pl>
2022-03-30 15:40:01 +0200

    More user-friendliness

--- scripts/opentims_verify.py
+++ scripts/opentims_verify.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
-import sys
 from tqdm import tqdm
 from pathlib import Path
 
 import opentimspy
 from opentimspy import OpenTIMS
 
-if opentimspy.bruker_bridge_present:
-    all_columns = ('frame','scan','tof','intensity','mz','inv_ion_mobility','retention_time')
-else:
-    all_columns = ('frame','scan','tof','intensity','retention_time')
-
 import argparse
 
-parser = argparse.ArgumentParser(description='Export a set of frames in TSV format.')
+parser = argparse.ArgumentParser(description='Verify the integrity of TDF dataset. Any errors could indicate corruption - or a bug in OpenTIMS.')
 parser.add_argument("path", help="TDF dataset path", type=Path)
 parser.add_argument("-c", "--convert", help="Perform tof->mz and scan->inv_ion_mobility conversions too", action="store_true")
 args=parser.parse_args()
@@ -28,3 +22,4 @@ with OpenTIMS(args.path) as D:
     # Iterate over frames. This will store only one frame at a time in RAM, preventing out of memory errors.
     for frame_id in tqdm(sorted(frames)):
         frame = D.query(frame_id, columns=all_columns)
+print("Success! File does not seem to be corrupted.")

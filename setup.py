98986c8296170f2c389b83ccc64345c90a93069a
Michal Startek <michal.startek@gmail.com>
2026-02-19 17:24:17 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -173,7 +173,7 @@ else:
 setup(
     name="opentimspy",
     packages=["opentimspy"],
-    version="1.0.18",
+    version="1.0.19",
     author="Mateusz Krzysztof Łącki (MatteoLacki), Michał Startek (michalsta)",
     author_email="matteo.lacki@gmail.com, michal.startek@mimuw.edu.pl",
     description="opentimspy: An open-source parser of Bruker Tims Data File (.tdf).",
